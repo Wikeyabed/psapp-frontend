8c209a2eabc49403950bee78bd2b04e83112806d
Warhammer704 <front.geek704@gmail.com>
2023-02-26 15:18:11 +0330

    material next boilrtplate

--- components/admin/login/LoginForm.js
+++
@@ -1,8 +0,0 @@
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
-
-function LoginForm() {
-  return <div>Login</div>;
-}
-
-export default LoginForm;
--- pages/admin/login.js
+++
@@ -1,4 +0,0 @@
-import LoginForm from "@/components/admin/login/LoginForm";
-export default function Login() {
-  return <LoginForm />;
-}
