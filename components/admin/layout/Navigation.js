c446ad62b60d76134295cbfb106c0bbe82ecfaf9
Warhammer704 <front.geek704@gmail.com>
2023-03-30 16:09:48 +0330

    add notification and user icon on navigation bar and changed breadcrumb

--- components/admin/layout/Navigation.js
+++ components/admin/layout/Navigation.js
@@ -1,16 +1,29 @@
 import * as React from "react";
-
-import { Box, Stack, Link, Typography, Breadcrumbs } from "@mui/material";
+import {
+  Box,
+  Stack,
+  Link,
+  Typography,
+  Breadcrumbs,
+  IconButton,
+  Menu,
+  MenuItem,
+  ListItemText,
+  Badge,
+} from "@mui/material";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 export default function NavigationBar() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
   const breadcrumbs = [
-    <Link underline="hover" key="1" color="inherit" href="/">
+    <Link underline="none" key="1" color="primary" href="/">
       داشبورد
     </Link>,
     <Link
-      underline="hover"
+      underline="none"
       key="2"
-      color="inherit"
+      color="primary"
       href="/material-ui/getting-started/installation/"
     >
       کاربران
@@ -20,19 +33,72 @@ export default function NavigationBar() {
     </Typography>,
   ];
 
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Box
       sx={{
-        minHeight: 64,
-        py: 2,
-        px: 3,
+        minHeight: 48, // made the navigation bar smaller
+        py: 1, // made the padding smaller
+        px: 2, // made the padding smaller
+        bgcolor: "#f5f5f5",
+        display: "flex",
+        justifyContent: "space-between", // aligned user icon to right
+        alignItems: "center",
+        color: "primary.contrastText",
+        boxShadow: "0px 2px 2px rgba(0,0,0,0.1)", // added shadow
       }}
     >
-      <Stack>
-        <Breadcrumbs separator="›" aria-label="breadcrumb">
-          {breadcrumbs}
-        </Breadcrumbs>
-      </Stack>
+      <Breadcrumbs sx={{ color: "#5f5f5f" }} aria-label="breadcrumb">
+        {breadcrumbs}
+      </Breadcrumbs>
+
+      <div>
+        <Badge
+          badgeContent={4}
+          color="error"
+          overlap="circle"
+          anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        >
+          <IconButton color="primary">
+            <NotificationsIcon />
+          </IconButton>
+        </Badge>
+        <IconButton
+          size="large"
+          edge="end"
+          color="primary" // changed icon color to blue
+          aria-label="profile"
+          sx={{ mr: 2 }}
+          onClick={handleMenuOpen}
+        >
+          <AccountCircleIcon />
+        </IconButton>
+        <Menu
+          id="menu-appbar"
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          open={Boolean(anchorEl)}
+          onClose={handleMenuClose}
+        >
+          <MenuItem onClick={handleMenuClose}>Account settings</MenuItem>
+          <MenuItem onClick={handleMenuClose}>Logout</MenuItem>
+        </Menu>
+      </div>
     </Box>
   );
 }
