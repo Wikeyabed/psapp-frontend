1be3a608c8c99b99fb26584a99b6300931ec5dd6
warhammer <front.geek704@gmail.com>
2023-08-08 12:44:05 +0330

    pages folder structure for blog and products

--- components/admin/blog/index.js
+++ components/admin/blog/index.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import { useState } from "react";
 import PropTypes from "prop-types";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
@@ -19,7 +19,7 @@ function CustomTabPanel(props) {
     >
       {value === index && (
         <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
+          <>{children}</>
         </Box>
       )}
     </div>
@@ -40,7 +40,7 @@ function a11yProps(index) {
 }
 
 export default function AdminBlog() {
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
