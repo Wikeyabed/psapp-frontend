e81d29226eeb4d4387aa0120484a91c122acf39e
warhammer <front.geek704@gmail.com>
2023-08-20 11:30:17 +0330

    /payment query string datas

--- pages/payment/fail/[tid].js
+++
@@ -1,8 +0,0 @@
-import FailedPayment from "../../../components/public/payments/FailedPayment";
-import { useRouter } from "next/router";
-
-export default function Fail() {
-  const router = useRouter();
-  const tid = router.query.tid;
-  return <FailedPayment tid={tid} />;
-}
