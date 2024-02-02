a13441b5cc9988ea3d03aef14a9ca94126b29243
warhammer <front.geek704@gmail.com>
2024-02-02 15:50:04 +0330

    torob headers for bot

--- pages/products/[productId].js
+++ pages/products/[productId].js
@@ -6,6 +6,19 @@ export default function ProductSingle({ product }) {
   return (
     <PublicLayout>
       <Head>
+        <meta name="product_id" content={product.product_id} />
+        <meta name="product_name" content={product.product_name} />
+        <meta
+          name="product_price"
+          content={product.price * (1 - product.discount * 0.01)}
+        />
+        <meta name="product_old_price" content={product.price} />
+        <meta
+          name="availability"
+          content={product.product_quantity > 0 ? "instock" : "outofstock"}
+        />
+        <meta property="og:image" content={product.images_url[0]} />
+
         <title>ایباکس - {product.product_name}</title>
       </Head>
       <ProductPage product={product} />
